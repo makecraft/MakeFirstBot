fbf46cc8fff1bc6c2caf9389611212e12e30bd3b
makecraft <makecraft@outlook.com>
2023-01-06 12:07:35 -0500

    act

--- MakeUser.py
+++ MakeUser.py
@@ -5,8 +5,6 @@ from pyrogram.handlers import MessageHandler
 # Create a new Client instance
 app = Client("MakeUserBot")
 
-await app.send_message("@xXACRVXx", "Hola **PY**")
-
 @app.on_message(filters.text & filters.private)
 async def echo(client, message):
     if message.startswith("reply"):
